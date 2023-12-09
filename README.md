b3857f90f1154646f66908bbb76790019da18781
Ryan Shine <ryan.c.shine@outlook.com>
2023-12-09 14:08:12 +0800

    Rock Paper Scissors game against a computer functioning with 5 rounds, scores being checked and determining a winner at the end all played in console.

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # rock-paper-scissors
 odin project task
 
-function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: You Lose, Paper beats Rock
- function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-Account for TIES by re-playing the round.
-
-Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
+Rock Paper Scissors game against a computer functioning with 5 rounds, scores being checked and determining a winner at the end all played in console.
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+    var playerPoints = 0;
+    var computerPoints = 0;
+   
 function getComputerChoice(){
     const random = (max, min) => 
     Math.floor(Math.random() * (3)) + 1;
@@ -12,88 +15,45 @@ function getComputerChoice(){
      return 'scissors'
    }
 }
-
-
+ 
 const str = round()
  function round() {
         const computerChoice = getComputerChoice()
-    console.log(computerChoice);
+   // console.log(computerChoice);
     const promptStr = prompt();
     const playerChoice = promptStr.toLowerCase();
+    
    
     if (playerChoice === 'rock' && computerChoice ==='scissors')
-    return console.log("You win, rock beats scissors");
+    return console.log("You win, rock beats scissors") + (playerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'rock' && computerChoice ==='paper')
-    return console.log("You lose, paper beats rock");
+    return console.log("You lose, paper beats rock") + (computerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'rock' && computerChoice === 'rock')
     return console.log("Draw, both picked rock") + round();
     else if (playerChoice === 'paper' && computerChoice === 'rock')
-    return console.log('You win, paper beats rock');
+    return console.log('You win, paper beats rock') + (playerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'paper' && computerChoice=== 'paper')
     return console.log('Draw, both picked paper') + round ();
     else if (playerChoice ==='paper' && computerChoice=== 'scissors')
-    return console.log('You lose, scissors beats paper');
+    return console.log('You lose, scissors beats paper')+ (computerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'scissors' && computerChoice=== 'rock')
-    return console.log('You lose, rock  beats scissors');
+    return console.log('You lose, rock  beats scissors')+ (computerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'scissors' && computerChoice=== 'paper')
-    return console.log('You win, scissors beats paper');
+    return console.log('You win, scissors beats paper') + (playerPoints ++) + console.log('You, '+playerPoints+' : Computer, '+computerPoints);
     else if (playerChoice === 'scissors' && computerChoice === 'scissors')
     return console.log('Draw, both picked scissors') + round();
 
-
-  
-
 }
 
-
-//console.log(str)
-/*
-function reset() {
-    let result = str.substring(0,5);
-    if (result==='Draw,')
-    return round();
-    else ''
+function game(){
+    if (playerPoints >= 3)
+    return "You beat the computer!!";
+    else if (computerPoints >= 3)
+    return "You lost to the computer :(";
 }
-    console.log(reset()) */
 
-
-
-/*
-function drawReset(){
-if (result ==='Draw, bo')
-return ;
-}
-const newComputerChoice = getComputerChoice();
-const newPlayerChoice =prompt();
-//console.log(newComputerChoice);
-//console.log(newPlayerChoice);
-function newRound(){
-    if (newPlayerChoice === 'rock' && newComputerChoice ==='scissors')
-    return console.log("You win, rock beats scissors";
-    else if (newPlayerChoice === 'rock' && newComputerChoice ==='paper')
-    return console.log("You lose, paper beats rock";
-    else if (newPlayerChoice === 'rock' && newComputerChoice === 'rock')
-    return "Draw, both picked rock";
-    else if (newPlayerChoice === 'paper' && newComputerChoice === 'rock')
-    return console.log('You win, paper beats rock';
-    else if (newPlayerChoice === 'paper' && newComputerChoice=== 'paper')
-    return 'Draw, both picked paper';
-    else if (newPlayerChoice ==='paper' && newComputerChoice=== 'scissors')
-    return console.log('You lose, scissors beats paper';
-    else if (newPlayerChoice === 'scissors' && newComputerChoice=== 'rock')
-    return console.log('You lose, rock  beats scissors';
-    else if (newPlayerChoice === 'scissors' && newComputerChoice=== 'paper')
-    return console.log('You win, scissors beats paper';
-    else if (newPlayerChoice === 'scissors' && newComputerChoice === 'scissors')
-    return 'Draw, both picked scissors';
-}
-console.log(newRound())
-
-
-
-//player selection 
-
-//computer selection
-//case insensetive
-//if player picks '' + computer selects '' then print ''
-*/
+round();
+round();
+round();
+round();
+console.log(game());
